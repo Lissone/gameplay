a401ee378bc7b714f36d0d24a6edc8620fbf0acb
Leonardo Dias Lissone Santomero <leonardo.lissonez@gmail.com>
2021-06-24 16:31:23 -0300

    user info in home profile

--- src/components/Profile/index.tsx
+++ src/components/Profile/index.tsx
@@ -4,14 +4,18 @@ import {
   Text
 } from 'react-native'
 
+import { useAuth } from '../../hooks/auth'
+
 import { Avatar } from '../Avatar'
 
 import { styles } from './styles'
 
 export function Profile() {
+  const { user } = useAuth()
+
   return (
     <View style={styles.container}>
-      <Avatar urlImage='https://github.com/Lissone.png'/>
+      <Avatar urlImage={user.avatar}/>
 
       <View>
         <View style={styles.user}>
@@ -20,7 +24,7 @@ export function Profile() {
           </Text>
 
           <Text style={styles.username}>
-            Leonardo
+            {user.firstName}
           </Text>
         </View>
 
